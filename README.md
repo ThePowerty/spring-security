6b4347eef953a23efc74138f429c29b9bd188a3f
Williams <w.infanzon.98@gmail.com>
2024-04-04 11:21:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Spring Security
+Proyecto para probar la dependencia de Spring Security
+1. Asignar valores para user y password en application.properties
+   * `spring.security.user.name=admin`
+   * `spring.security.user.password=1234` 
+   * Default user = user | Default password = {auto generated for spring}
+2. Crear un Security Config para añadir filtros de seguridad
---
+++ src/main/java/com/williams/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.williams.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(Customizer.withDefaults()) // en caso de usar formularios no es recomendable
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/hola").permitAll() // filtro de endpoints
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
